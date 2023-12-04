37e2a505d6d1e58ac0f051c438a73def88f2a5f5
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2023-12-05 02:44:01 +0900

    More tests

--- test/embedding/test_ae_feature_map.py
+++ test/embedding/test_ae_feature_map.py
@@ -28,12 +28,12 @@ from qdna.embedding import AeFeatureMap
 
 class TestAeFeatureMap(TestCase):
 
-    def test_state(self):
+    def test_rnd_state(self):
         n_qubits = 6
-        state_vector = np.random.rand(2**n_qubits)
+        state_vector = (np.random.rand(2**n_qubits)-0.5)*2
         state_vector = state_vector / np.linalg.norm(state_vector)
 
-        feature_map = AeFeatureMap(n_qubits)
+        feature_map = AeFeatureMap(n_qubits, normalize=False, set_global_phase=True)
 
         circuit = QuantumCircuit(n_qubits)
         circuit.compose(feature_map, list(range(n_qubits)), inplace=True)
@@ -41,4 +41,43 @@ class TestAeFeatureMap(TestCase):
 
         state = Statevector(circuit)
 
+        self.assertTrue(np.allclose(state_vector, state))
+
+    def test_non_normalized_state(self):
+        # This test is important because during NQE training
+        # the parameter vector is not normalized.
+        n_qubits = 4
+        state_vector = [
+            -3.1416, 0.0000, -0.7667, 0.5835, 0.3468, 0.5320, 0.6706, 0.6756,
+            0.5270, 0.7115, 0.5165, 0.7194, -0.5392, 0.3878, 0.5628, 0.7304
+        ]
+
+        feature_map = AeFeatureMap(n_qubits, normalize=True, set_global_phase=True)
+
+        circuit = QuantumCircuit(n_qubits)
+        circuit.compose(feature_map, list(range(n_qubits)), inplace=True)
+        circuit.assign_parameters(state_vector, inplace=True)
+
+        state = Statevector(circuit)
+
+        state_vector = state_vector / np.linalg.norm(state_vector)
+        self.assertTrue(np.allclose(state_vector, state))
+
+    def test_sparse_state(self):
+        # Ensure that embedding is preventing division by zero.
+        n_qubits = 4
+        state_vector = [
+            0.0000, 0.0000, 0.0000, 0.5835, 0.0000, 0.5320, 0.6706, 0.6756,
+            0.5270, 0.0000, 0.0000, 0.7194, 0.0000, 0.0000, 0.0000, 0.7304
+        ]
+
+        feature_map = AeFeatureMap(n_qubits, normalize=True, set_global_phase=False)
+
+        circuit = QuantumCircuit(n_qubits)
+        circuit.compose(feature_map, list(range(n_qubits)), inplace=True)
+        circuit.assign_parameters(state_vector, inplace=True)
+
+        state = Statevector(circuit)
+
+        state_vector = state_vector / np.linalg.norm(state_vector)
         self.assertTrue(np.allclose(state_vector, state))
