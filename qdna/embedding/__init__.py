aeb0025e56773673843b7d0c48640e3ec29de0ce
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2024-03-24 21:21:53 +0900

    refactoring

--- qdna/embedding/__init__.py
+++ qdna/embedding/__init__.py
@@ -1,7 +1,7 @@
-from qdna.embedding.iqp_feature_map import IqpFeatureMap
-from qdna.embedding.ae_feature_map import AeFeatureMap
+from .iqp_feature_map import IqpFeatureMap
+from .ae_feature_map import AeFeatureMap
 
-from qdna.embedding.nqe_z_feature_map import NqeZFeatureMap
-from qdna.embedding.nqe_zz_feature_map import NqeZZFeatureMap
-from qdna.embedding.nqe_iqp_feature_map import NqeIqpFeatureMap
-from qdna.embedding.nqe_ae_feature_map import NqeAeFeatureMap
+from .nqe_z_feature_map import NqeZFeatureMap
+from .nqe_zz_feature_map import NqeZZFeatureMap
+from .nqe_iqp_feature_map import NqeIqpFeatureMap
+from .nqe_ae_feature_map import NqeAeFeatureMap
